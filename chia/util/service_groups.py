5f09f88a1251025883eadfb80a6fb840a6566561
Waruta <fgreen5.extra@gmail.com>
2022-09-23 21:03:10 +1000

    fix service group names

--- chia/util/service_groups.py
+++ chia/util/service_groups.py
@@ -2,25 +2,25 @@ from typing import KeysView, Generator
 
 SERVICES_FOR_GROUP = {
     "all": (
-        "chia_harvester chia_timelord_launcher chia_timelord chia_farmer "
-        "chia_full_node chia_wallet chia_data_layer chia_data_layer_http"
+        "moon_harvester moon_timelord_launcher moon_timelord moon_farmer "
+        "moon_full_node moon_wallet moon_data_layer moon_data_layer_http"
     ).split(),
     # TODO: should this be `data_layer`?
-    "data": "chia_wallet chia_data_layer".split(),
-    "data_layer_http": "chia_data_layer_http".split(),
+    "data": "moon_wallet moon_data_layer".split(),
+    "data_layer_http": "moon_data_layer_http".split(),
     "node": "moon_full_node".split(),
     "harvester": "moon_harvester".split(),
-    "farmer": "chia_harvester chia_farmer chia_full_node chia_wallet".split(),
-    "farmer-no-wallet": "chia_harvester chia_farmer chia_full_node".split(),
+    "farmer": "moon_harvester moon_farmer moon_full_node moon_wallet".split(),
+    "farmer-no-wallet": "moon_harvester moon_farmer moon_full_node".split(),
     "farmer-only": "moon_farmer".split(),
-    "timelord": "chia_timelord_launcher chia_timelord chia_full_node".split(),
+    "timelord": "moon_timelord_launcher moon_timelord moon_full_node".split(),
     "timelord-only": "moon_timelord".split(),
     "timelord-launcher-only": "moon_timelord_launcher".split(),
     "wallet": "moon_wallet".split(),
     "introducer": "moon_introducer".split(),
     "simulator": "moon_full_node_simulator".split(),
     "crawler": "moon_crawler".split(),
-    "seeder": "chia_crawler chia_seeder".split(),
+    "seeder": "moon_crawler moon_seeder".split(),
     "seeder-only": "moon_seeder".split(),
 }
 
