a86049ece0f6e887c42d308b1afd488d913f911d
Waruta <fgreen5.extra@gmail.com>
2022-09-23 20:09:47 +1000

    multiprocessing limit

--- chia/consensus/blockchain.py
+++ chia/consensus/blockchain.py
@@ -48,6 +48,8 @@ from chia.util.generator_tools import get_block_header, tx_removals_and_addition
 from chia.util.inline_executor import InlineExecutor
 from chia.util.ints import uint16, uint32, uint64, uint128
 from chia.util.setproctitle import getproctitle, setproctitle
+from chia.util.default_root import DEFAULT_ROOT_PATH
+from chia.util.config import load_config
 
 log = logging.getLogger(__name__)
 
@@ -130,7 +132,10 @@ class Blockchain(BlockchainInterface):
             cpu_count = multiprocessing.cpu_count()
             if cpu_count > 61:
                 cpu_count = 61  # Windows Server 2016 has an issue https://bugs.python.org/issue26903
+            config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
             num_workers = max(cpu_count - reserved_cores, 1)
+            if 'multiprocessing_limit' in config.keys():
+                num_workers = min(num_workers, int(config["multiprocessing_limit"]));
             self.pool = ProcessPoolExecutor(
                 max_workers=num_workers,
                 mp_context=multiprocessing_context,
